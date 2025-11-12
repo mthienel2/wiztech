57cd2fe7c9230010d3d0f1f9d93b6d2e6580c3a3
Max <mthienel226@gmail.com>
2025-11-12 12:56:59 -0500

    Project structure

---
+++ app/about/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>This is the about page!</div>;
+};
+
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <nav className="flex gap-2">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </nav>
         {children}
       </body>
     </html>
